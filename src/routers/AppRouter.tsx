84cee665f4214359e64eaadc8c37b153a601f70a
Nick Broeks <nick.broeks@live.nl>
2023-03-12 14:31:55 +0100

    Added a HomePage and a router path to it

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import HomePage from '../modules/home/pages/HomePage';
 import NotFound from '../pages/NotFound';
 
 
 export default function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   )
